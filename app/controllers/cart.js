b8ad1994d2b8610c9ee669074709e01522885c81
Ikkaku Fuzii <ultimate100commit@users.noreply.github.com>
2015-03-06 01:50:48 -0500

    update CartController

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  uniqProductCount: function() {
+    return this.mapBy('id').uniq().get('length');
+  }.property('@length')
 });

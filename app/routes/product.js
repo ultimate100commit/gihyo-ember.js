d6cf77f8e6432048b599840958593f038afe37b5
Ikkaku Fuzii <ultimate100commit@users.noreply.github.com>
2015-03-06 01:39:32 -0500

    add addCart action in product.hbs

--- app/routes/product.js
+++ app/routes/product.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.modelFor('products').findBy('id', Number(params.id));
+  },
+
+  actions: {
+    addCart: function(product) {
+      this.controllerFor('cart').pushObject(product);
+
+      this.transitionTo('cart');
+    }
   }
 });

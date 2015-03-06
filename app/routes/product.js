b96501babc68e1a7d923bf80f5c1d04a601828f0
Ikkaku Fuzii <ultimate100commit@users.noreply.github.com>
2015-03-06 05:49:21 -0500

    update addCart action

--- app/routes/product.js
+++ app/routes/product.js
@@ -8,6 +8,7 @@ export default Ember.Route.extend({
   actions: {
     addCart: function(product) {
       this.controllerFor('cart').pushObject(product);
+      this.controllerFor('cart').save();
 
       this.transitionTo('cart');
     }

47545d4cd5d5f53c301b56182b15ef4875db08fc
Ikkaku Fuzii <ultimate100commit@users.noreply.github.com>
2015-03-06 01:59:46 -0500

    add totalPrice property in CartController

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  totalPrice: function() {
+    return this.mapBy('price').reduce(function(total, price) {
+      return total + price;
+    }, 0);
+  }.property('@each.price'),
+
   uniqProductCount: function() {
     return this.mapBy('id').uniq().get('length');
   }.property('@length')
